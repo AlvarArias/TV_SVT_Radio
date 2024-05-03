aac95f30579f4b31ae4573a0b6a4ae9480030b53
Alvar Arias <alvararias@gmail.com>
2024-05-03 12:26:25 +0200

    Load images from JSON VStack for images and text

--- TV_SVT_Radio/ContentView.swift
+++ TV_SVT_Radio/ContentView.swift
@@ -6,21 +6,69 @@
 //
 
 import SwiftUI
+import CachedAsyncImage
+
 
 struct ContentView: View {
+    
+    // Load Stations
+    var mYradioStation = LoadRadioStationJSONFile()
+    
+    @State var radioStations: [radioStationInfo] = []
+
+    let items = 0...51
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color.orange
+                .ignoresSafeArea()
+            if radioStations.count > 0 {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 20) {
+                        
+                            ForEach(items, id: \.self) { item in
+                                VStack {
+                                CachedAsyncImage(url: URL(string: radioStations[item].image), content: { image in
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 200, height: 200)
+                                        .cornerRadius(10)
+                                }, placeholder: {
+                                    ProgressView()
+                                })
+                                   
+                                    Text(radioStations[item].name)
+                                    .frame(width: 200, height: 100)
+                                    .background(Color.blue.opacity(0.5))
+                                    .cornerRadius(10)
+                                    .focusable(true)
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                }
+            }
+            .onAppear {
+            
+            if radioStations.isEmpty {
+                radioStations = mYradioStation.loadStation()
+            }
+            
         }
-        .padding()
+            
+        
     }
 }
+
+
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
