9a424b426e44d19d51f772c4cad524e8ce16c44a
Alvar Arias <alvararias@gmail.com>
2024-05-03 12:03:38 +0200

    Initial Commit

---
+++ TV_SVT_Radio/TV_SVT_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_SVT_RadioApp.swift
+//  TV_SVT_Radio
+//
+//  Created by Alvar Arias on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct TV_SVT_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TV_SVT_RadioUITests/TV_SVT_RadioUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TV_SVT_RadioUITestsLaunchTests.swift
+//  TV_SVT_RadioUITests
+//
+//  Created by Alvar Arias on 2024-05-03.
+//
+
+import XCTest
+
+final class TV_SVT_RadioUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
