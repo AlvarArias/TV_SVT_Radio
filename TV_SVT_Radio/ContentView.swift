5a1e4086fc383db98535e5ed419aa333aa301075
Alvar Arias <alvararias@gmail.com>
2024-05-09 22:26:41 +0200

    Nombre, imagen y texto dinámico respecto a la radio seleccionada

--- TV_SVT_Radio/ContentView.swift
+++ TV_SVT_Radio/ContentView.swift
@@ -20,8 +20,20 @@ struct ContentView: View {
     @State var radioStations: [radioStationInfo] = []
     
     @State var isPlaying = false
+    
+    @State var urlRadioStationHome : String = "http://sverigesradio.se/topsy/direkt/srapi/132.mp3"
+    
+    @State var  urlImageRadioSelected : String = "https://static-cdn.sr.se/images/132/2186745_512_512.jpg"
+    
+    @State var nameRadio : String = "P1"
+    
+    @State var descriptionRadio : String = "Talat innehåll om samhälle, kultur och vetenskap. Kanalen erbjuder nyheter och aktualiteter, granskning och fördjupning men också livsåskådnings-och livsstilsprogram samt underhållning och upplevelser till exempel i form av teater."
+    
+    @State var isFocused = false
+    
    
-
+    
+   
     let items = 0...51
     
     var body: some View {
@@ -29,51 +41,86 @@ struct ContentView: View {
         ZStack {
             Color.orange
                 .ignoresSafeArea()
-            if radioStations.count > 0 {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack(spacing: 20) {
+            VStack {
+                             
+                    if radioStations.count > 0 {
+                       
+                        HStack {
+                            
+                            
+                            RadioStationImage(url: URL(string: urlImageRadioSelected), accssibilityLabel: nameRadio)
+                            .frame(width: 200, height: 200)
+                            .padding(.top, 50)
+                            
+                            
+                            DetailView(text: nameRadio, textDescription: descriptionRadio, urlRadioStationHome: urlRadioStationHome, buttonIsPlaying: isPlaying)
+                            
+                            
+                        }
                         
-                            ForEach(items, id: \.self) { item in
-                                VStack {
-                                    
-                                    Button(action: {
-                                        
-                                        print("Tapped \(radioStations[item].name)")
-                                       
-                                        isPlaying.toggle()
-                                        isPlaying = playRadio.playSongRadio(radioURL: radioStations[item].url, isPlaying: isPlaying)
-                                        print("isPlaying: \(isPlaying)")
-                                        
-                                    }) {
-                                        VStack {
-                                            RadioStationImage(url: URL(string: radioStations[item].image) , accssibilityLabel: radioStations[item].name)
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            
+                            LazyHStack(spacing: 20) {
+
+                                ForEach(items, id: \.self) { item in
+                                    VStack {
+
+                                        Button(action: {
                                             
-                                            Text(radioStations[item].name)
-                                                .font(.headline)
-                                                .foregroundColor(.white)
-                                                .padding(.top, 10)
+                                            print("Tapped \(radioStations[item].name)")
                                             
-                                        }
-                                    }
+                                            isPlaying.toggle()
+                                            
+                                            isPlaying = playRadio.playSongRadio(radioURL: radioStations[item].url, isPlaying: isPlaying)
+                                            
+                                            print("isPlaying: \(isPlaying)")
+                                            
+                                            urlImageRadioSelected = radioStations[item].image
+                                            
+                                            nameRadio = radioStations[item].name
+                                            
+                                            descriptionRadio = radioStations[item].tagline
+                                            
+                                        }){
+                                            VStack {
+                                                RadioStationImage(url: URL(string: radioStations[item].image) , accssibilityLabel: radioStations[item].name)
+                                                    .frame(width: 200, height: 200)
                                 
+                                                  
+                                                
+                                                Text(radioStations[item].name)
+                                                    .font(.headline)
+                                                    .foregroundColor(.white)
+                                                    .padding(.top, 10)
+                                                
+                                            }
+                                        }
+                                        
+                                    }
+                                }
                             }
+                            .padding(.horizontal)
                         }
                     }
-                    .padding(.horizontal)
+                
+                
+               
+                
                 }
+                .onAppear {
+                    
+                    if radioStations.isEmpty {
+                        radioStations = mYradioStation.loadStation()
+                    }
+                    
                 }
+            
             }
-            .onAppear {
-            
-            if radioStations.isEmpty {
-                radioStations = mYradioStation.loadStation()
-            }
-            
-        }
-            
         
+        }
     }
-}
+
 
 struct RadioStationImage: View {
     let url: URL?
@@ -87,23 +134,75 @@ struct RadioStationImage: View {
                 .cornerRadius(10)
                 .focusable(true)
                 .shadow(radius: 10)
-                //.accessibility(label: Text(radioStations[item].name))
                 .accessibility(label: Text(accssibilityLabel ?? "no information"))
+            
             
         }, placeholder: {
             ProgressView()
         })
+       
     }
 }
     
+struct DetailView: View {
+    
+    var text: String?
+    
+    var textDescription: String?
+    
+    var isPlaying = false
+    
+    var urlRadioStationHome : String = ""
+    
+   var playRadio = PlayRadio()
+    
+    @State var buttonIsPlaying : Bool = false
 
-/*
-  Text(radioStations[item].name)
-  .frame(width: 200, height: 100)
-  .background(Color.blue.opacity(0.5))
-  .cornerRadius(10)
-  .focusable(true)
-*/
+    var body: some View {
+        
+        
+        VStack(alignment: .leading) {
+                
+                
+            Text(text ?? "")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.leading, 10)
+                    .padding(.top, 10)
+                
+                Text(textDescription ?? "")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.leading, 10)
+                    .padding(.bottom, 10)
+                    .frame(width: 600, alignment: .leading)
+                    
+                
+                
+                Button(action: {
+                    
+                    buttonIsPlaying.toggle()
+                    buttonIsPlaying = playRadio.playSongRadio(radioURL: urlRadioStationHome, isPlaying: buttonIsPlaying)
+                    print("buttonIsPlaying: \(buttonIsPlaying)")
+                    
+                }) {
+                    
+                    Image(systemName: buttonIsPlaying ? "pause.fill" : "play.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                        .padding(.leading, 10)
+                        .focusable(true)
+                }
+                
+                
+            }
+
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- TV_SVT_Radio/Model/radioStationInfo.swift
+++ TV_SVT_Radio/Model/radioStationInfo.swift
@@ -9,23 +9,7 @@
 import Foundation
 import UIKit
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let myChannellRAdio = try? newJSONDecoder().decode(MyChannellRAdio.self, from: jsonData)
-// file radios.json
-// informacion sacada de :
-// https://api.sr.se/api/v2/channels
-// https://api.sr.se/v2/channels?page=2 (proxima página)
 
-/*
- 
-A struct that represents a radio station, containing various properties such as name, image, and URL.
-
-Usage: Initialize an instance of DemoRadio with the desired properties.
-
-Note: This struct conforms to the Hashable and Codable protocols.
-*/
 
 struct radioStationInfo: Hashable, Codable  {
 
